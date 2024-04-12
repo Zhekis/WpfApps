caf00ff08964539e237e25bc093cc902bdfad1ed
Zhekis <zheki4@gmail.com>
2024-04-12 15:30:38 +0500

    Добавьте файлы проекта.

---
+++ ViewModels/ImageWebLoadVM .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using WpfApp1.Ifrastructure;
+
+namespace WpfApp1.ViewModels
+{
+    internal class ImageWebLoadVM : OnPropertyChangedClass
+    {
+        static ImageSourceConverter imageSourceConverter = new ImageSourceConverter();
+
+        private ImageSource webImage;
+        private RelayCommand _loadImageCommand;
+
+        public ImageSource WebImage { get => webImage; set { webImage = value; OnPropertyChanged(); } }
+        public RelayCommand LoadImageCommand => _loadImageCommand ?? (_loadImageCommand = new RelayCommand(LoadImageMetod, LoadImageCanMetod));
+
+        /// <summary>Проверка допустимости пути - не реализована.
+        /// Стоит "заглушка</summary>
+        private bool LoadImageCanMetod(object parameter) => true;
+
+        private void LoadImageMetod(object parameter)
+        {
+            if (LoadImageCanMetod(parameter))
+            {
+                WebImage = (ImageSource)imageSourceConverter.ConvertFrom(parameter);
+            }
+        }
+    }
+}
